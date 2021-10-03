e5a660e27463efbfb95613ac430cbc5b18886044
David Brown <davidbrown@Davids-MacBook-Pro-2.local>
2021-10-03 14:30:08 -0400

    created models for note manager. created database. added superuser.'

--- asst/note_manager/models.py
+++ asst/note_manager/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# table to store list of categories
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+# table to store individual notes
+#class Post(models.Model):
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True) # auto_now_add assigns curr data/time to this field whenever instance is created 
+    last_modified = models.DateTimeField(auto_now=True) # auto_now assigns curr date/time to field whenever instance is SAVED (edits)
+    #categories = models.ManyToManyField('Category', related_name='posts') # link categories/notes (many-to-many)
+    categories = models.ManyToManyField('Category', related_name='notes') # link categories/notes (many-to-many) using djangos ManytoManyField type 
+
+#
+class Comment(models.Model):
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    #post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    note = models.ForeignKey('Note', on_delete=models.CASCADE)
